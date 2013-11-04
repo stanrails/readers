2f8f5b03305aa3863d49a36e09945def77a47a06
stanrails <stansmith@pushingsocial.com>
2013-11-03 23:31:58 -0500

    debug

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module First100readers
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
-config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+
   end
 end
