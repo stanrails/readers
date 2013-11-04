7e11392aa4bfd9554603a88c6cf3db0752ad2315
stanrails <stansmith@pushingsocial.com>
2013-11-04 17:44:40 -0500

    precompile debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ First100readers::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
@@ -20,17 +20,17 @@ First100readers::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
@@ -76,6 +76,9 @@ First100readers::Application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-
+config.cache_classes = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
  
 end
