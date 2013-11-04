7cca2abdf5fb7a7fc6021abe337d70bd5a23f5bf
stanrails <stansmith@pushingsocial.com>
2013-11-04 18:22:16 -0500

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,6 @@ config.cache_classes = true
 config.serve_static_assets = true
 config.assets.compile = true
 config.assets.digest = true
-config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+config.assets.precompile = ['*.js', '*.css','style.css', '*.css.erb']
  
 end
