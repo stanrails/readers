7da2f39430d086fd1ad05ed7e97aa03cf9f62c52
stanrails <stansmith@pushingsocial.com>
2013-11-04 21:38:52 -0500

    debug

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,11 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 First100readers::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['app19196288@heroku.com'],
-  :password       => ENV['mbc72zla'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
+config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: "plain",
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: 'heroku.com'
+  }
