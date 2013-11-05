b306ab4470f714039368e3176a9eafdfab3a626e
stanrails <stansmith@pushingsocial.com>
2013-11-04 21:30:08 -0500

    change sendgrid permissions

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['pushingsocial'],
-  :password       => ENV['tammy1228'],
+  :user_name      => ENV['app19196288@heroku.com'],
+  :password       => ENV['mbc72zla'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
