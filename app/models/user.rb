deee97107ac086b0e38056a92d1a4ffe4fd46a53
stanrails <stansmith@pushingsocial.com>
2013-11-05 22:20:18 -0500

    Added autocrop for paperclip uploads

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
 	self.inheritance_column = nil
 
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "150x150#", 
+  	:thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
 	end
