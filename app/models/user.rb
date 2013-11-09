38146e438f87dff6fe3c0293be9ef543f1bd9595
stanrails <stansmith@pushingsocial.com>
2013-11-09 16:17:34 -0500

    add avatar error validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
 self.inheritance_column = nil
 
   encrypted_id key: '1vA3tza3g5GOg7EOE7ubEL'
+
   has_attached_file :avatar, :styles => { :medium => "150x150#", 
-  	:thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
-	end
+  	:thumb => "100x100#" }, :default_url => "/images/:style/no-avatar.png"
+  validates_attachment_presence :avatar
+
+end
